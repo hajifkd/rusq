229ef3091046a3cae7ba76619d2239509b3b5b1d
Hajime Fukuda <hajime.fukuda@me.com>
2018-02-03 16:31:08 +0900

    removed unnecessary codes

--- src/lib.rs
+++ src/lib.rs
@@ -24,11 +24,3 @@ pub trait QuantumMachine {
     fn get_qubits(&self) -> Vec<Qubit>;
     fn measure(&mut self, qubit: &Qubit) -> MeasuredResult;
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,7 @@
 pub use simulator::QuantumSimulator;
 pub use QuantumMachine;
 pub use MeasuredResult;
+pub use Qubit;
 
 pub use gates::single::SingleGateApplicator;
 pub use gates::double::DoubleGateApplicator;
-pub use super::Qubit;
