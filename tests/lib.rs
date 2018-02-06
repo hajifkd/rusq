0f79718cbcf7b3c0250b66fc2ba9177690397e9b
Hajime Fukuda <hajime.fukuda@me.com>
2018-02-06 23:18:25 +0900

    test for permutations

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,30 +1,55 @@
+extern crate permutohedron;
 extern crate rusq;
 
 use rusq::prelude::*;
+use permutohedron::LexicalPermutation;
 
+macro_rules! count {
+    () => { 0 };
+
+    ($e1: expr) => { 1 };
+
+    ($e1: expr, $($e: expr),+) => {
+        1 + count!($($e),*)
+    };
+}
+
+// Well, is this too complicated as test?
+// Maybe we need "examples" in addition.
 macro_rules! logic {
     ( ($sim: ident, $qubits: ident, $gate: ident) => {} ) => {};
 
     ( ($sim: ident, $qubits: ident, $gate: ident) => { $($input: expr),+ => $($result: expr),+ ; $($e: tt)* } ) => {
         {
-            let mut qubits_iter = $qubits.iter();
-            $({
-                let qubit = qubits_iter.next().unwrap();
-                set(&mut $sim, qubit, if $input == 0 { MeasuredResult::Zero } else { MeasuredResult::One });
-            });*
+            let mut indices = (0..count!($($input),*)).collect::<Vec<usize>>();
 
-            let mut index = -1;
-            $sim.$gate($({
-                index += 1;
-                let _ = $input;
-                &$qubits[index as usize]
-            }),*);
+            loop {
+                let iperm = indices.to_vec();
+                if !indices.next_permutation() {
+                    break;
+                }
 
-            let mut qubits_iter = $qubits.iter();
-            $({
-                let qubit = qubits_iter.next().unwrap();
-                assert_eq!(if $result == 0 { MeasuredResult::Zero } else { MeasuredResult::One }, $sim.measure(qubit));
-            });*
+                let mut index = -1;
+                $({
+                    index += 1;
+                    set(&mut $sim, &$qubits[iperm[index as usize]],
+                        if $input == 0 { MeasuredResult::Zero } else { MeasuredResult::One });
+                });*
+
+                let mut index = -1;
+                $sim.$gate($({
+                    index += 1;
+                    let _ = $input;
+                    &$qubits[iperm[index as usize]]
+                }),*);
+
+                let mut index = -1;
+                $({
+                    index += 1;
+                    assert_eq!(if $result == 0 { MeasuredResult::Zero } else { MeasuredResult::One },
+                               $sim.measure(&$qubits[iperm[index as usize]]));
+                });*
+            }
         }
 
         logic!(($sim, $qubits, $gate) => { $($e)* })
@@ -89,6 +114,23 @@ fn operate_h() {
 }
 
 #[test]
+fn epr_pair() {
+    let mut sim = rusq::simulator::QuantumSimulator::new(2);
+    let qubits = sim.get_qubits();
+    let measure_count = 10000;
+
+    for _ in 0..measure_count {
+        set(&mut sim, &qubits[0], MeasuredResult::Zero);
+        set(&mut sim, &qubits[1], MeasuredResult::Zero);
+
+        sim.H(&qubits[0]);
+        sim.CNOT(&qubits[0], &qubits[1]);
+
+        assert_eq!(sim.measure(&qubits[0]), sim.measure(&qubits[1]));
+    }
+}
+
+#[test]
 fn operate_cnot() {
     let mut sim = rusq::simulator::QuantumSimulator::new(2);
     let qubits = sim.get_qubits();
@@ -112,23 +154,6 @@ fn operate_swap() {
         1, 0 => 0, 1;
         1, 1 => 1, 1;
     });
-}
-
-#[test]
-fn epr_pair() {
-    let mut sim = rusq::simulator::QuantumSimulator::new(2);
-    let qubits = sim.get_qubits();
-    let measure_count = 10000;
-
-    for _ in 0..measure_count {
-        set(&mut sim, &qubits[0], MeasuredResult::Zero);
-        set(&mut sim, &qubits[1], MeasuredResult::Zero);
-
-        sim.H(&qubits[0]);
-        sim.CNOT(&qubits[0], &qubits[1]);
-
-        assert_eq!(sim.measure(&qubits[0]), sim.measure(&qubits[1]));
-    }
 }
 
 #[test]
